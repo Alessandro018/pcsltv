733114f2b7ca8957dc609cb0df813db0665ec31a
Alessandro018 <alessandrosilva325@gmail.com>
2020-07-09 18:34:54 -0300

    Tela de login

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+</head>
+<body>
+	<div class="content">
+		<div class="login">
+			<form  action="">
+				<h2>Acessar sua conta</h2>
+				<label >Login</label>
+				<input type="text" name="login">
+				<label >Senha</label>
+				<input type="password" name="senha">
+				<button>Entrar</button>
+			</form>
+
+		</div>
+	</div>
+</body>
+<link rel="stylesheet" type="text/css" href="app/css/style.css">
+</html>
