c205a734b75414089956db95ff9ed4babf153e98
Alessandro018 <alessandrosilva325@gmail.com>
2020-07-09 20:06:07 -0300

    Layout da aplicação

--- index.php
+++ index.php
@@ -5,18 +5,16 @@
 	<title>Login</title>
 </head>
 <body>
-	<div class="content">
-		<div class="login">
-			<form  action="">
-				<h2>Acessar sua conta</h2>
-				<label >Login</label>
-				<input type="text" name="login">
-				<label >Senha</label>
-				<input type="password" name="senha">
-				<button>Entrar</button>
-			</form>
+	<div class="login">
+		<form  action="">
+			<h2>Acessar sua conta</h2>
+			<label >Login</label>
+			<input type="text" name="login">
+			<label >Senha</label>
+			<input type="password" name="senha">
+			<button>Entrar</button>
+		</form>
 
-		</div>
 	</div>
 </body>
 <link rel="stylesheet" type="text/css" href="app/css/style.css">
