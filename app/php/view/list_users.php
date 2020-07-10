e108513f172d9f8567274d74870ced1417a13272
Alessandro018 <alessandrosilva325@gmail.com>
2020-07-10 19:52:39 -0300

    Tela de listagem de clientes

---
+++ app/php/view/list_users.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<div class="container">
+		<div class="sidenav">
+			<a href="">Clientes</a>
+			<a href="">Produtos</a>
+			<a href="">Fornecedores</a>
+		</div>
+		<div class="status">
+			<label for="">Welcome Alessandro!</label>
+		</div>
+		<div class="content">
+			<div class="list-users">
+				<div class="row">
+					<label>Lista de clientes</label>
+					<a>Cadastrar</a>
+				</div>
+				<table>
+					<tr>
+						<td>Nome de cara</td>
+						<td><a href="">Ver</a></td>
+						<td><a href="">Editar</a></td>
+						<td><a href="">Excluir</a></td>
+					</tr>
+					<tr>
+						<td>Nome de cara</td>
+						<td><a href="">Ver</a></td>
+						<td><a href="">Editar</a></td>
+						<td><a href="">Excluir</a></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+	<link rel="stylesheet" href="../../css/style.css"/>
+</body>
+</html>
